775a1ae926628e7370c22213cf3b8e643387cd37
AlexAhmanHV <alexander.ahman@student.hv.se>
2025-08-05 16:06:29 +0200

    Ditt commit-meddelande här

--- Models/Team.cs
+++ Models/Team.cs
@@ -1,12 +1,18 @@
-﻿public class Team
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+public class Team
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // 👈 detta är det viktiga
     public int Id { get; set; }
+
     public int RoundId { get; set; }
     public int TeamNumber { get; set; }
     public string TeamType { get; set; }
     public DateTime CreatedAt { get; set; }
 
-    public Round? Round { get; set; } = null; // ✅ optional
-    public ICollection<TeamPlayer> TeamPlayers { get; set; } = new List<TeamPlayer>(); // ✅ initierad
-    public TeamScore? TeamScore { get; set; } = null; // ✅ optional
+    public Round? Round { get; set; } = null;
+    public ICollection<TeamPlayer> TeamPlayers { get; set; } = new List<TeamPlayer>();
+    public TeamScore? TeamScore { get; set; } = null;
 }
